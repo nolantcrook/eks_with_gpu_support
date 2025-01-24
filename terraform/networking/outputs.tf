9b125cf9521c13bdb7190f34d96dadab0fa68d1b
Nolan Crook <nolancrook@macbookpro.lan>
2025-01-24 00:12:31 -0700

    updating jenkinfile to destroy in reverse order

--- terraform/networking/outputs.tf
+++ terraform/networking/outputs.tf
@@ -16,4 +16,14 @@ output "private_subnet_ids" {
 output "cluster_security_group_id" {
   description = "Security group ID for the cluster"
   value       = aws_security_group.cluster.id
-} 
+}
+
+# output "argocd_security_group_id" {
+#   description = "Security group ID for ArgoCD"
+#   value       = aws_security_group.argocd.id
+# }
+
+# output "argocd_alb_dns_name" {
+#   description = "DNS name of the ArgoCD ALB"
+#   value       = aws_lb.argocd.dns_name
+# } 
---
+++ terraform/networking/variables.tf
@@ -0,0 +1,4 @@
+# variable "route53_zone_id" {
+#   description = "Route53 hosted zone ID for DNS validation"
+#   type        = string
+# } 
