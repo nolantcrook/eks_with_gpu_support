77b65d6b686dffb3ad6bdde11ff8c45e510e5a40
Nolan Crook <nolancrook@macbookpro.lan>
2025-01-24 23:14:27 -0700

    increasing the number of pods

--- terraform/compute/nodes.tf
+++ terraform/compute/nodes.tf
@@ -11,8 +11,8 @@ resource "aws_eks_node_group" "arm" {
   ]
 
   scaling_config {
-    desired_size = 1
-    max_size     = 2
+    desired_size = 2
+    max_size     = 3
     min_size     = 1
   }
 
