42aa0034d72bf7de7147e1b43a080665f5fe6375
Nolan Crook <nolancrook@macbookpro.lan>
2025-01-27 07:56:40 -0700

    updating node group instnace types

--- terraform/compute/nodes.tf
+++ terraform/compute/nodes.tf
@@ -1,6 +1,6 @@
-resource "aws_eks_node_group" "x86" {
+resource "aws_eks_node_group" "x86_spot" {
   cluster_name    = aws_eks_cluster.eks_gpu.name
-  node_group_name = "eks-x86-1"
+  node_group_name = "eks-x86-spot"
   node_role_arn   = aws_iam_role.node.arn
   subnet_ids      = local.private_subnet_ids
   capacity_type   = "SPOT"
@@ -9,7 +9,18 @@ resource "aws_eks_node_group" "x86" {
     "t3.medium",
     "t3.large",
     "t3.xlarge",
-    "m5.large"
+    "t3a.medium",
+    "t3a.large",
+    "t3a.xlarge",
+    "m5.medium",
+    "m5.large",
+    "m5.xlarge",
+    "m5a.medium",
+    "m5a.large",
+    "m5a.xlarge",
+    "m6i.medium",
+    "m6i.large",
+    "m6i.xlarge"
   ]
 
   scaling_config {
@@ -34,10 +45,48 @@ resource "aws_eks_node_group" "x86" {
   }
 
   tags = {
-    Name        = "eks-x86-${var.environment}"
+    Name        = "eks-x86-spot-${var.environment}"
     Environment = var.environment
     "k8s.io/cluster-autoscaler/enabled" = "true"
     "k8s.io/cluster-autoscaler/node-template/label/lifecycle" = "Ec2Spot"
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
+    aws_iam_role_policy_attachment.node_secrets_access,
+  ]
+}
+
+resource "aws_eks_node_group" "x86_ondemand" {
+  cluster_name    = aws_eks_cluster.eks_gpu.name
+  node_group_name = "eks-x86-ondemand"
+  node_role_arn   = aws_iam_role.node.arn
+  subnet_ids      = local.private_subnet_ids
+  capacity_type   = "ON_DEMAND"
+  
+  instance_types = [
+    "t3.medium",
+    "t3.large",
+    "t3.xlarge",
+    "m5.large"
+  ]
+
+  scaling_config {
+    desired_size = 0
+    max_size     = 5
+    min_size     = 0
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  tags = {
+    Name        = "eks-x86-ondemand-${var.environment}"
+    Environment = var.environment
+    "k8s.io/cluster-autoscaler/enabled" = "true"
   }
 
   depends_on = [
