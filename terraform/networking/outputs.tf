f4d06edfd0e3d5459387d1bba93c80f1dd0dd48f
Nolan Crook <nolancrook@macbookpro.lan>
2025-01-30 19:55:46 -0700

    updating networking outputs

--- terraform/networking/outputs.tf
+++ terraform/networking/outputs.tf
@@ -26,4 +26,29 @@ output "argocd_security_group_id" {
 output "argocd_alb_dns_name" {
   description = "DNS name of the ArgoCD ALB"
   value       = aws_lb.argocd.dns_name
+}
+
+output "certificate_arn" {
+  description = "ARN of the ACM certificate"
+  value       = aws_acm_certificate.domain.arn
+}
+
+output "waf_acl_arn" {
+  description = "ARN of the WAF ACL"
+  value       = aws_wafv2_web_acl.main.arn
+}
+
+output "alb_security_group_id" {
+  description = "ID of the ALB security group"
+  value       = aws_security_group.alb.id
+}
+
+resource "aws_ssm_parameter" "argocd_ingress_params" {
+  name  = "/eks/${var.environment}/argocd/ingress"
+  type  = "SecureString"
+  value = jsonencode({
+    certificate_arn      = aws_acm_certificate.domain.arn
+    waf_acl_arn         = aws_wafv2_web_acl.main.arn
+    alb_security_group_id = aws_security_group.alb.id
+  })
 } 
