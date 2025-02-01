2c793812ab0b33b3dc91ee7e506760a49cbbac9b
Nolan Crook <nolancrook@macbookpro.lan>
2025-02-01 11:25:13 -0700

    adding asg as target of target group

--- terraform/compute/data.tf
+++ terraform/compute/data.tf
@@ -20,11 +20,11 @@ locals {
   private_subnet_ids        = data.terraform_remote_state.networking.outputs.private_subnet_ids
   public_subnet_ids         = data.terraform_remote_state.networking.outputs.public_subnet_ids
   cluster_security_group_id = data.terraform_remote_state.networking.outputs.cluster_security_group_id
-  alb_target_group_arn      = data.terraform_remote_state.networking.outputs.alb_target_group_arn
 
   # Get ASG names from both node groups
-  ondemand_asg_name = aws_eks_node_group.x86_ondemand.resources[0].autoscaling_groups[0].name
-  spot_asg_name     = aws_eks_node_group.x86_spot.resources[0].autoscaling_groups[0].name
+  ondemand_asg_name    = aws_eks_node_group.x86_ondemand.resources[0].autoscaling_groups[0].name
+  spot_asg_name        = aws_eks_node_group.x86_spot.resources[0].autoscaling_groups[0].name
+  alb_target_group_arn = data.terraform_remote_state.networking.outputs.alb_target_group_arn
 }
 
 # Create attachments for both ASGs
@@ -33,32 +33,8 @@ resource "aws_autoscaling_attachment" "eks_ondemand_asg_attachment" {
   lb_target_group_arn    = local.alb_target_group_arn
 }
 
-resource "aws_lb_target_group" "argocd" {
-  name        = "argocd-${var.environment}"
-  port        = 80
-  protocol    = "HTTP"
-  vpc_id      = data.terraform_remote_state.networking.outputs.vpc_id
-  target_type = "instance"
-
-  health_check {
-    enabled             = true
-    healthy_threshold   = 2
-    interval            = 15
-    matcher             = "200"
-    path                = "/"
-    port                = "traffic-port"
-    protocol            = "HTTP"
-    timeout             = 5
-    unhealthy_threshold = 2
-  }
-}
 
 resource "aws_autoscaling_attachment" "eks_spot_asg_attachment_spot" {
   autoscaling_group_name = local.spot_asg_name
-  lb_target_group_arn    = aws_lb_target_group.argocd.arn
-}
-
-resource "aws_autoscaling_attachment" "eks_spot_asg_attachment_ondemand" {
-  autoscaling_group_name = local.ondemand_asg_name
-  lb_target_group_arn    = aws_lb_target_group.argocd.arn
+  lb_target_group_arn    = local.alb_target_group_arn
 }
--- terraform/networking/alb.tf
+++ terraform/networking/alb.tf
@@ -108,7 +108,7 @@ resource "aws_lb_target_group" "argocd" {
   port        = 80
   protocol    = "HTTP"
   vpc_id      = aws_vpc.main.id
-  target_type = "ip"
+  target_type = "instance"
 
   health_check {
     enabled             = true
