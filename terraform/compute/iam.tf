b26a0796ad88b57f104064091aa543b894afad11
Nolan Crook <nolancrook@macbookpro.lan>
2025-02-08 00:14:47 -0700

    updating iam

--- terraform/compute/iam.tf
+++ terraform/compute/iam.tf
@@ -46,22 +46,22 @@ resource "aws_iam_role_policy" "alb_controller" {
   })
 }
 
-# Create service account for the ALB controller
-resource "kubernetes_service_account" "alb_controller" {
-  depends_on = [aws_eks_cluster.eks_gpu]
+# # Create service account for the ALB controller
+# resource "kubernetes_service_account" "alb_controller" {
+#   depends_on = [aws_eks_cluster.eks_gpu]
 
-  metadata {
-    name      = "aws-load-balancer-controller"
-    namespace = "kube-system"
-    annotations = {
-      "eks.amazonaws.com/role-arn" = aws_iam_role.alb_controller.arn
-    }
-  }
+#   metadata {
+#     name      = "aws-load-balancer-controller"
+#     namespace = "kube-system"
+#     annotations = {
+#       "eks.amazonaws.com/role-arn" = aws_iam_role.alb_controller.arn
+#     }
+#   }
 
-  lifecycle {
-    ignore_changes = [
-      metadata[0].annotations,
-      metadata[0].labels,
-    ]
-  }
-}
+#   lifecycle {
+#     ignore_changes = [
+#       metadata[0].annotations,
+#       metadata[0].labels,
+#     ]
+#   }
+# }
