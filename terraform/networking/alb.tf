80f81dc9cbc3f23570106c1799f5bd90e9c31704
Nolan Crook <nolancrook@macbookpro.lan>
2025-02-08 17:29:15 -0700

    updating alb listener for new cert

--- terraform/networking/alb.tf
+++ terraform/networking/alb.tf
@@ -159,12 +159,16 @@ resource "aws_lb_listener" "argocd" {
   port              = "443"
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
-  certificate_arn   = aws_acm_certificate.argocd.arn
+  certificate_arn   = aws_acm_certificate_validation.argocd.certificate_arn
 
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.argocd.arn
   }
+
+  depends_on = [
+    aws_acm_certificate_validation.argocd
+  ]
 
   lifecycle {
     create_before_destroy = true
