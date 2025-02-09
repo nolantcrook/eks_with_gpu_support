d9a6afaa46f4073c22d3056699a2fb893c0200f4
Nolan Crook <nolancrook@macbookpro.lan>
2025-02-08 17:20:50 -0700

    adding dns record

--- terraform/networking/alb.tf
+++ terraform/networking/alb.tf
@@ -1,10 +1,10 @@
 # Certificate
 resource "aws_acm_certificate" "argocd" {
-  domain_name       = "argocd.hello-world-domain.com"
+  domain_name       = "*.hello-world-domain.com"
   validation_method = "DNS"
 
   tags = {
-    Name = "argocd-cert"
+    Name = "wildcard-cert"
   }
 
   lifecycle {
@@ -202,6 +202,40 @@ resource "aws_lb_listener_rule" "argocd" {
   condition {
     host_header {
       values = ["argocd.hello-world-domain.com"]
+    }
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# Add new Route53 record for 2048
+resource "aws_route53_record" "game_2048" {
+  zone_id = local.route53_zone_id
+  name    = "2048.hello-world-domain.com"
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.argocd.dns_name
+    zone_id                = aws_lb.argocd.zone_id
+    evaluate_target_health = true
+  }
+}
+
+# Add listener rule for 2048
+resource "aws_lb_listener_rule" "game_2048" {
+  listener_arn = aws_lb_listener.argocd.arn
+  priority     = 200
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.argocd.arn
+  }
+
+  condition {
+    host_header {
+      values = ["2048.hello-world-domain.com"]
     }
   }
 
