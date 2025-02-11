c335ff6fa417a79ec385a2179e3eee299c9a484c
Nolan Crook <nolancrook@macbookpro.lan>
2025-02-10 22:28:06 -0700

    reverting

--- terraform/networking/security_groups.tf
+++ terraform/networking/security_groups.tf
@@ -9,17 +9,6 @@ resource "aws_security_group_rule" "alb_to_nginx" {
   description              = "Allow ALB to NGINX Ingress NodePort"
 }
 
-# ALB Egress to Cluster
-resource "aws_security_group_rule" "alb_to_cluster_egress" {
-  type                     = "egress"
-  from_port                = 0
-  to_port                  = 65535
-  protocol                 = "tcp"
-  source_security_group_id = aws_security_group.cluster.id
-  security_group_id        = aws_security_group.argocd.id
-  description              = "Allow ALB to send traffic to cluster"
-}
-
 # Cluster internal traffic
 resource "aws_security_group_rule" "cluster_internal" {
   type                     = "ingress"
