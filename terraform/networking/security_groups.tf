c387af61ee6d4af9d22527abaf057329179c13f4
Nolan Crook <nolancrook@macbookpro.lan>
2025-02-10 22:41:44 -0700

    updating security group rules

--- terraform/networking/security_groups.tf
+++ terraform/networking/security_groups.tf
@@ -45,15 +45,6 @@ resource "aws_security_group" "argocd" {
     description = "Allow HTTPS traffic"
   }
 
-  # Allow all egress
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "Allow all outbound traffic"
-  }
-
   tags = {
     Name        = "argocd-${var.environment}"
     Environment = var.environment
