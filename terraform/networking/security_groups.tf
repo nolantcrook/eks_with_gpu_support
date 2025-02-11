797bddc2ee1faed04f3eaf2336ada8a1657743d9
Nolan Crook <nolancrook@macbookpro.lan>
2025-02-10 22:31:34 -0700

    reverting

--- terraform/networking/security_groups.tf
+++ terraform/networking/security_groups.tf
@@ -10,15 +10,15 @@ resource "aws_security_group_rule" "alb_to_nginx" {
 }
 
 # Cluster internal traffic
-resource "aws_security_group_rule" "cluster_internal" {
-  type                     = "ingress"
-  from_port                = 0
-  to_port                  = 65535
-  protocol                 = "tcp"
-  source_security_group_id = aws_security_group.cluster.id
-  security_group_id        = aws_security_group.cluster.id
-  description              = "Allow internal cluster traffic"
-}
+# resource "aws_security_group_rule" "cluster_internal" {
+#   type                     = "ingress"
+#   from_port                = 0
+#   to_port                  = 65535
+#   protocol                 = "tcp"
+#   source_security_group_id = aws_security_group.cluster.id
+#   security_group_id        = aws_security_group.cluster.id
+#   description              = "Allow internal cluster traffic"
+# }
 
 
 # Security Groups
