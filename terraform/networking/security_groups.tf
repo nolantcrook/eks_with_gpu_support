766741ead93a82efe30bed97b662593ec8d6ffdf
Nolan Crook <nolancrook@macbookpro.lan>
2025-02-10 23:45:22 -0700

    updating security group rules

--- terraform/networking/security_groups.tf
+++ terraform/networking/security_groups.tf
@@ -64,16 +64,16 @@ resource "aws_security_group" "cluster" {
   }
 }
 
-# Cluster internal traffic
-resource "aws_security_group_rule" "cluster_internal" {
-  type                     = "ingress"
-  from_port                = 0
-  to_port                  = 65535
-  protocol                 = "tcp"
-  source_security_group_id = aws_security_group.cluster.id
-  security_group_id        = aws_security_group.cluster.id
-  description              = "Allow internal cluster traffic"
-}
+# # Cluster internal traffic
+# resource "aws_security_group_rule" "cluster_internal" {
+#   type                     = "ingress"
+#   from_port                = 0
+#   to_port                  = 65535
+#   protocol                 = "tcp"
+#   source_security_group_id = aws_security_group.cluster.id
+#   security_group_id        = aws_security_group.cluster.id
+#   description              = "Allow internal cluster traffic"
+# }
 
 # ALB to NodePort (for NGINX Ingress)
 resource "aws_security_group_rule" "alb_to_nginx" {
