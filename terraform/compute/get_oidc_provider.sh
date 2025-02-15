3b0c59bf720895b59503c5bd5e6d08aafdf7e892
Nolan Crook <ncrook104@gmail.com>
2025-02-14 18:18:23 -0700

    updating oidc shell script for thumbprint

--- terraform/compute/get_oidc_provider.sh
+++ terraform/compute/get_oidc_provider.sh
@@ -1,13 +1,26 @@
    # get_oidc_provider.sh
    #!/bin/bash
+
+   # Debug: Print the cluster name
+   echo "Cluster Name: $1"
+
    CLUSTER_NAME=$1
    OIDC_URL=$(aws eks describe-cluster --name "$CLUSTER_NAME" --query "cluster.identity.oidc.issuer" --output text)
+
+   # Debug: Print the OIDC URL
+   echo "OIDC URL: $OIDC_URL"
 
    # Extract the domain from the OIDC URL
    OIDC_DOMAIN=$(echo "$OIDC_URL" | sed 's|^https://||')
 
+   # Debug: Print the OIDC Domain
+   echo "OIDC Domain: $OIDC_DOMAIN"
+
    # Fetch the thumbprint using openssl
    THUMBPRINT=$(echo | openssl s_client -connect ${OIDC_DOMAIN}:443 2>/dev/null | openssl x509 -fingerprint -noout | cut -d'=' -f2 | sed 's/://g')
+
+   # Debug: Print the Thumbprint
+   echo "Thumbprint: $THUMBPRINT"
 
    # Output the OIDC URL and thumbprint in JSON format
    echo "{\"oidc_url\": \"$OIDC_URL\", \"thumbprint\": \"$THUMBPRINT\"}"
