0801b16c7aa98862fb4e30c2529b446df3414176
Nolan Crook <ncrook104@gmail.com>
2025-02-15 00:09:34 -0700

    updating oidc url

--- terraform/compute/get_oidc_provider.sh
+++ terraform/compute/get_oidc_provider.sh
@@ -13,8 +13,11 @@
    # Correctly extract the domain from the OIDC URL
    OIDC_DOMAIN=$(echo "$OIDC_URL" | sed -E 's|^https://([^/]+).*|\1|')
 
+   # Remove "https://" from the OIDC URL
+   OIDC_URL_NO_HTTPS=$(echo "$OIDC_URL" | sed 's|^https://||')
+
    # Redirect debug output to stderr
-   echo "OIDC Domain: $OIDC_DOMAIN" >&2
+   echo "OIDC URL without https: $OIDC_URL_NO_HTTPS" >&2
 
    # Fetch the thumbprint using openssl
    THUMBPRINT=$(echo | openssl s_client -connect ${OIDC_DOMAIN}:443 2>/dev/null | openssl x509 -fingerprint -noout | cut -d'=' -f2 | sed 's/://g')
@@ -22,5 +25,5 @@
    # Redirect debug output to stderr
    echo "Thumbprint: $THUMBPRINT" >&2
 
-   # Output the OIDC URL and thumbprint in JSON format
-   echo "{\"oidc_url\": \"$OIDC_URL\", \"thumbprint\": \"$THUMBPRINT\"}"
+   # Output the OIDC URL without "https://" and thumbprint in JSON format
+   echo "{\"oidc_url\": \"$OIDC_URL_NO_HTTPS\", \"thumbprint\": \"$THUMBPRINT\"}"
