aa59cb47903af605c95d3a2ca009bd0620c8af73
Nolan Crook <ncrook104@gmail.com>
2025-02-14 23:00:34 -0700

    fixing tag typo

--- terraform/compute/nodes.tf
+++ terraform/compute/nodes.tf
@@ -48,7 +48,7 @@ resource "aws_eks_node_group" "x86_spot" {
     Environment                                               = var.environment
     "k8s.io/cluster-autoscaler/enabled"                       = "true"
     "k8s.io/cluster-autoscaler/node-template/label/lifecycle" = "Ec2Spot"
-    "k8s.io/cluster-autoscaler/eku-gpu-${var.environment}"    = "owned"
+    "k8s.io/cluster-autoscaler/eks-gpu-${var.environment}"    = "owned"
   }
 
   launch_template {
@@ -97,7 +97,7 @@ resource "aws_eks_node_group" "x86_ondemand" {
     Name                                                   = "eks-x86-ondemand-${var.environment}"
     Environment                                            = var.environment
     "k8s.io/cluster-autoscaler/enabled"                    = "true"
-    "k8s.io/cluster-autoscaler/eku-gpu-${var.environment}" = "owned"
+    "k8s.io/cluster-autoscaler/eks-gpu-${var.environment}" = "owned"
   }
 
   depends_on = [
