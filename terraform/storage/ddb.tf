ef7fe9d8922e9e042cf5deb3990ebcb1cbd822b5
Nolan Crook <ncrook104@gmail.com>
2025-03-28 13:50:38 -0600

    adding auth codes ddb table

--- terraform/storage/ddb.tf
+++ terraform/storage/ddb.tf
@@ -8,3 +8,14 @@ resource "aws_dynamodb_table" "generate_image_status" {
     type = "S"
   }
 }
+
+resource "aws_dynamodb_table" "invokeai_auth_codes" {
+  name         = "invokeai_auth_codes"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "code"
+
+  attribute {
+    name = "code"
+    type = "S"
+  }
+}
