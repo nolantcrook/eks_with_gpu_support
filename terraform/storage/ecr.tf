6a67d547fc02756158694a9fbdeae9103cd0acf0
Nolan Crook <ncrook104@gmail.com>
2025-03-31 22:37:58 -0600

    adding ecr for invokeai api

--- terraform/storage/ecr.tf
+++ terraform/storage/ecr.tf
@@ -65,3 +65,71 @@ resource "aws_ecr_lifecycle_policy" "flask_api" {
     ]
   })
 }
+
+# ECR Repository for InvokeAI Headless
+resource "aws_ecr_repository" "invokeai_headless" {
+  name                 = "invokeai-headless"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  encryption_configuration {
+    encryption_type = "KMS"
+  }
+
+  tags = {
+    Name        = "invokeai-headless-${var.environment}"
+    Environment = var.environment
+  }
+}
+
+# ECR Repository Policy for InvokeAI Headless
+resource "aws_ecr_repository_policy" "invokeai_headless" {
+  repository = aws_ecr_repository.invokeai_headless.name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "AllowPushPull"
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        }
+        Action = [
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:PutImage",
+          "ecr:InitiateLayerUpload",
+          "ecr:UploadLayerPart",
+          "ecr:CompleteLayerUpload"
+        ]
+      }
+    ]
+  })
+}
+
+# ECR Lifecycle Policy for InvokeAI Headless
+resource "aws_ecr_lifecycle_policy" "invokeai_headless" {
+  repository = aws_ecr_repository.invokeai_headless.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Keep last 5 images"
+        selection = {
+          tagStatus   = "any"
+          countType   = "imageCountMoreThan"
+          countNumber = 5
+        }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
