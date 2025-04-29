192bd2547700bb697ecfd32adc9266a527faca8b
Nolan Crook <ncrook104@gmail.com>
2025-04-28 23:09:06 -0600

    adding deepseek website

--- terraform/networking/website_setups.tf
+++ terraform/networking/website_setups.tf
@@ -12,6 +12,20 @@ module "argocd_setup" {
   listener_arn         = aws_lb_listener.eks_alb.arn
 }
 
+
+module "deepseek_setup" {
+  source               = "./cognito_website_setup"
+  environment          = var.environment
+  website_name         = "deepseek"
+  website_domain       = local.route53_zone_name
+  route53_zone_id      = local.route53_zone_id
+  priority             = 600
+  alb_target_group_arn = aws_lb_target_group.eks_alb.arn
+  alb_dns_name         = aws_lb.eks_alb.dns_name
+  alb_zone_id          = aws_lb.eks_alb.zone_id
+  listener_arn         = aws_lb_listener.eks_alb.arn
+}
+
 locals {
   website_setups = {
     game_2048 = {
