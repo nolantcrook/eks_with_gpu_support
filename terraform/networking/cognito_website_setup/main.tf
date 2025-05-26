001bf4790a674730dac4eabad6664ea0e1fd4580
Nolan Crook <ncrook104@gmail.com>
2025-05-25 23:36:06 -0600

    updating cognito website name to be dynamic

--- terraform/networking/cognito_website_setup/main.tf
+++ terraform/networking/cognito_website_setup/main.tf
@@ -25,7 +25,7 @@ resource "aws_cognito_user_pool" "website" {
   verification_message_template {
     default_email_option = "CONFIRM_WITH_CODE"
     email_message        = "Your verification code is {####}"
-    email_subject        = "Verify your email for InvokeAI"
+    email_subject        = "Verify your email for ${var.website_name}"
   }
 
   # Email configuration using Cognito's default email service
