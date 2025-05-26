9acafd48ec3a2d39e99171fdb9751bf194e69e4d
Nolan Crook <ncrook104@gmail.com>
2025-05-25 20:40:50 -0600

    adding ecr, iam, website setups

--- terraform/networking/website_setups.tf
+++ terraform/networking/website_setups.tf
@@ -26,6 +26,20 @@ module "agent_demo_setup" {
 }
 
 
+
+module "tco_demo_setup" {
+  source               = "./cognito_website_setup"
+  website_name         = "tco-demo"
+  website_domain       = local.route53_zone_name
+  route53_zone_id      = local.route53_zone_id
+  priority             = 1100
+  alb_target_group_arn = aws_lb_target_group.eks_alb.arn
+  alb_dns_name         = aws_lb.eks_alb.dns_name
+  alb_zone_id          = aws_lb.eks_alb.zone_id
+  listener_arn         = aws_lb_listener.eks_alb.arn
+}
+
+
 locals {
   website_setups = {
     game_2048 = {
