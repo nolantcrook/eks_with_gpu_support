a44f728004c7b92a9b3c5915f3ed038a89ca89d4
Nolan Crook <ncrook104@gmail.com>
2025-06-06 01:44:36 -0600

    parameterizing access policy

--- terraform/rag/main.tf
+++ terraform/rag/main.tf
@@ -120,8 +120,9 @@ resource "aws_opensearchserverless_access_policy" "knowledge_base" {
           ResourceType = "index"
         }
       ]
-      Principal = [
-        "*"
+      Principals = [
+        aws_iam_role.bedrock_knowledge_base_role.arn,
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
       ]
     }
   ])
