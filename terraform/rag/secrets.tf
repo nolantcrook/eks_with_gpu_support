4285b6dc8bf4d8a2833f18d33fb22303a030d550
Nolan Crook <ncrook104@gmail.com>
2025-06-08 21:25:21 -0600

    fixing secret string

--- terraform/rag/secrets.tf
+++ terraform/rag/secrets.tf
@@ -5,8 +5,8 @@ data "aws_secretsmanager_secret" "knowledge_base_id" {
 
 # Update the secret with the actual knowledge base ID
 resource "aws_secretsmanager_secret_version" "knowledge_base_id" {
-  secret_id     = data.aws_secretsmanager_secret.knowledge_base_id.arn
-  secret_string = var.knowledge_base_name
+  secret_id     = data.aws_secretsmanager_secret.knowledge_base_id.id
+  secret_string = aws_bedrockagent_knowledge_base.main.id
 }
 
 output "knowledge_base_secret_arn" {
