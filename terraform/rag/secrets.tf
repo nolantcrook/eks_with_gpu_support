825b87885bb395b7aaa253fe330a7dfc90fcf9ef
Nolan Crook <ncrook104@gmail.com>
2025-06-08 19:16:55 -0600

    adding rag secrets for tracking bedrock id

---
+++ terraform/rag/secrets.tf
@@ -0,0 +1,15 @@
+# Data source to reference the secret created in foundation
+data "aws_secretsmanager_secret" "knowledge_base_id" {
+  name = "rag-project-knowledge-base-id"
+}
+
+# Update the secret with the actual knowledge base ID
+resource "aws_secretsmanager_secret_version" "knowledge_base_id" {
+  secret_id     = data.aws_secretsmanager_secret.knowledge_base_id.id
+  secret_string = var.knowledge_base_name
+}
+
+output "knowledge_base_secret_arn" {
+  description = "ARN of the secret containing the knowledge base ID"
+  value       = data.aws_secretsmanager_secret.knowledge_base_id.arn
+}
