ad44d20ee3d97c5e8160c0ec1dc67a8a2befd916
Nolan Crook <ncrook104@gmail.com>
2025-06-28 00:19:28 -0600

    removing acm

--- terraform/networking/acm_stratis.tf
+++ terraform/networking/acm_stratis.tf
@@ -1,38 +1,38 @@
-# Certificate
-resource "aws_acm_certificate" "hosted_zone_acm_certificate_stratis" {
-  domain_name               = data.aws_route53_zone.hosted_zone_stratis.name          # Apex domain
-  subject_alternative_names = ["*.${data.aws_route53_zone.hosted_zone_stratis.name}"] # Wildcard as SAN
-  validation_method         = "DNS"
+# # Certificate
+# resource "aws_acm_certificate" "hosted_zone_acm_certificate_stratis" {
+#   domain_name               = data.aws_route53_zone.hosted_zone_stratis.name          # Apex domain
+#   subject_alternative_names = ["*.${data.aws_route53_zone.hosted_zone_stratis.name}"] # Wildcard as SAN
+#   validation_method         = "DNS"
 
-  tags = {
-    Name = "wildcard-cert"
-  }
+#   tags = {
+#     Name = "wildcard-cert"
+#   }
 
-  lifecycle {
-    create_before_destroy = true
-  }
-}
+#   lifecycle {
+#     create_before_destroy = true
+#   }
+# }
 
-# DNS validation record
-resource "aws_route53_record" "cert_validation_stratis" {
-  for_each = {
-    for dvo in aws_acm_certificate.hosted_zone_acm_certificate_stratis.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  }
+# # DNS validation record
+# resource "aws_route53_record" "cert_validation_stratis" {
+#   for_each = {
+#     for dvo in aws_acm_certificate.hosted_zone_acm_certificate_stratis.domain_validation_options : dvo.domain_name => {
+#       name   = dvo.resource_record_name
+#       record = dvo.resource_record_value
+#       type   = dvo.resource_record_type
+#     }
+#   }
 
-  allow_overwrite = true
-  name            = each.value.name
-  records         = [each.value.record]
-  ttl             = 60
-  type            = each.value.type
-  zone_id         = local.route53_zone_id_stratis
-}
+#   allow_overwrite = true
+#   name            = each.value.name
+#   records         = [each.value.record]
+#   ttl             = 60
+#   type            = each.value.type
+#   zone_id         = local.route53_zone_id_stratis
+# }
 
-# Certificate validation
-resource "aws_acm_certificate_validation" "hosted_zone_acm_certificate_validation_stratis" {
-  certificate_arn         = aws_acm_certificate.hosted_zone_acm_certificate_stratis.arn
-  validation_record_fqdns = [for record in aws_route53_record.cert_validation_stratis : record.fqdn]
-}
+# # Certificate validation
+# resource "aws_acm_certificate_validation" "hosted_zone_acm_certificate_validation_stratis" {
+#   certificate_arn         = aws_acm_certificate.hosted_zone_acm_certificate_stratis.arn
+#   validation_record_fqdns = [for record in aws_route53_record.cert_validation_stratis : record.fqdn]
+# }
