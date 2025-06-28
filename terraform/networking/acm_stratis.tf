9ad81329eb96b2d1f9aae38d471281681049605b
Nolan Crook <ncrook104@gmail.com>
2025-06-28 00:09:48 -0600

    adding acm for stratis

--- terraform/networking/acm_stratis.tf
+++ terraform/networking/acm_stratis.tf
@@ -16,7 +16,7 @@ resource "aws_acm_certificate" "hosted_zone_acm_certificate_stratis" {
 # DNS validation record
 resource "aws_route53_record" "cert_validation_stratis" {
   for_each = {
-    for dvo in aws_acm_certificate.hosted_zone_acm_certificate.domain_validation_options : dvo.domain_name => {
+    for dvo in aws_acm_certificate.hosted_zone_acm_certificate_stratis.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
       record = dvo.resource_record_value
       type   = dvo.resource_record_type
