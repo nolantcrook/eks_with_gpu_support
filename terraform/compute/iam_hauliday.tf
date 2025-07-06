911e1fb76874404d01f656380e80e2bdc8a7c2bf
Nolan Crook <ncrook104@gmail.com>
2025-07-06 00:53:50 -0600

    fixing namespace for irsa role

--- terraform/compute/iam_hauliday.tf
+++ terraform/compute/iam_hauliday.tf
@@ -44,7 +44,7 @@ resource "aws_iam_role" "hauliday_role" {
         "Action" : "sts:AssumeRoleWithWebIdentity",
         "Condition" : {
           "StringLike" : {
-            "${replace(aws_iam_openid_connect_provider.eks_oidc.url, "https://", "")}:sub" : "system:serviceaccount:hauliday-backend:hauliday-backend-sa"
+            "${replace(aws_iam_openid_connect_provider.eks_oidc.url, "https://", "")}:sub" : "system:serviceaccount:hauliday:hauliday-backend-sa"
           }
         }
       }
