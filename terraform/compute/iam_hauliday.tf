ad673021a2aa2df6fa504baf35c91f74054efe57
Nolan Crook <ncrook104@gmail.com>
2025-07-05 23:34:16 -0600

    adding iam permisisons for hauliday app

---
+++ terraform/compute/iam_hauliday.tf
@@ -0,0 +1,58 @@
+resource "aws_iam_policy" "hauliday_policy" {
+  name        = "HaulidayPolicy"
+  description = "IAM policy for Hauliday API to access SQS and DynamoDB"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "sqs:SendMessage",
+          "sqs:ReceiveMessage",
+          "sqs:DeleteMessage",
+          "sqs:GetQueueAttributes"
+        ]
+        Resource = "arn:aws:sqs:us-west-2:891377073036:*" # Replace with your SQS queue ARN
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "dynamodb:GetItem",
+          "dynamodb:PutItem",
+          "dynamodb:Scan",
+          "dynamodb:Query"
+        ]
+        Resource = [                                                            # Replace with your DynamoDB table ARN
+          "arn:aws:dynamodb:us-west-2:891377073036:table/hauliday_reservations" # Replace with your DynamoDB table ARN
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role" "hauliday_role" {
+  name = "eks-hauliday-role"
+
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Federated" : "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/${aws_iam_openid_connect_provider.eks_oidc.url}"
+        },
+        "Action" : "sts:AssumeRoleWithWebIdentity",
+        "Condition" : {
+          "StringLike" : {
+            "${aws_iam_openid_connect_provider.eks_oidc.url}:sub" : "system:serviceaccount:hauliday-backend:hauliday-backend-sa"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "hauliday_policy_attach" {
+  role       = aws_iam_role.hauliday_role.name
+  policy_arn = aws_iam_policy.hauliday_policy.arn
+}
--- terraform/storage/ddb.tf
+++ terraform/storage/ddb.tf
@@ -19,3 +19,38 @@ resource "aws_dynamodb_table" "invokeai_auth_codes" {
     type = "S"
   }
 }
+
+resource "aws_dynamodb_table" "hauliday_reservations" {
+  name         = "hauliday_reservations"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "reservation_id"
+  range_key    = "start_date"
+
+  attribute {
+    name = "reservation_id"
+    type = "S"
+  }
+
+  attribute {
+    name = "start_date"
+    type = "S"
+  }
+
+  attribute {
+    name = "equipment_id"
+    type = "S"
+  }
+
+  global_secondary_index {
+    name            = "EquipmentDateIndex"
+    hash_key        = "equipment_id"
+    range_key       = "start_date"
+    projection_type = "ALL"
+  }
+
+  tags = {
+    Name        = "Hauliday Reservations"
+    Environment = "production"
+    Service     = "hauliday"
+  }
+}
